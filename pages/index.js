6e080f18ceee562b157c40a73428c87e144345da
laysabmedeiros <laysamedeiros.pro@gmail.com>
2026-02-24 21:15:11 +0000

    modifiquei mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-        return <h1>Eu te amo, mas você me estressa</h1>
+        return <h1>Oii! Eu aprendi a subir um site pra rede! Está no ar :) </h1>
 }
 
 export default Home;
